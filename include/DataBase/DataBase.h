16d94dee33032b26c38528398a638457a891d822
LingNc <1020449099@qq.com>
2024-12-16 22:56:28 +0800

    新增DataBase.h头文件，定义数据库结构体；删除DataStream.h和main.h头文件；在Vector.c中添加错误处理信息

---
+++ include/DataBase/DataBase.h
@@ -0,0 +1,17 @@
+#ifndef _database_lib
+#define _database_lib
+
+#include<stdio.h>
+#include"Tools/Vector.h"
+#include"Tools/String.h"
+#include"Student.h"
+#include"Book.h"
+
+typedef struct DataBase DataBase;
+typedef DataBase *dataBase;
+
+struct Database{
+
+};
+
+#endif
