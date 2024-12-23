3d9bf8abb71cb480f13954fcde5b8657eb87c90b
LingNc <1020449099@qq.com>
2024-12-23 13:34:07 +0800

    添加借书和还书功能，新增相关UI组件和函数；优化学生信息处理逻辑；增强代码可读性和功能性

--- include/ui/student_menu.h
+++ include/ui/student_menu.h
@@ -2,5 +2,8 @@
 #define _student_menu_h
 
 void student_menu(void *arg);
+void student_preInfo(void *arg);
+void student_postInfo(void *arg);
+void view_borrow_info(void **arg);
 
 #endif // _student_menu_h
--- src/ui/admin_book.c
+++ src/ui/admin_book.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <wchar.h>
 #include "ui/components/menu.h"
 #include "ui/components/func.h"
