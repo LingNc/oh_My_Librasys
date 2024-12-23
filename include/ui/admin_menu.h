50598b50375f3d8f8b300885fbe6ed68c79a583c
LingNc <1020449099@qq.com>
2024-12-23 20:28:41 +0800

    新增图书、管理员和学生管理功能的编辑和菜单接口；增强代码结构

--- include/ui/admin_menu.h
+++ include/ui/admin_menu.h
@@ -3,8 +3,12 @@
 
 void admin_menu(void *arg);
 void admin_preInfo(void *arg);
+void admin_postInfo(void *arg); // 新增
 void add_manager(void *arg);
+void edit_manager(void *arg); // 新增
 void delete_manager(void *arg);
+void view_manager_list(void *arg); // 新增
+void manager_menu(void *arg); // 新增
 void change_password(void *arg);
 
 #endif // _admin_menu_h
